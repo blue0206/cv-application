1239203b55289939a764a534000f31f3a1958382
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-17 17:23:30 +0530

    Add index file to export components for easier imports

---
+++ src/components/index.ts
@@ -0,0 +1,17 @@
+import Input from "./Input";
+import Label from "./Label";
+import Button from "./Button";
+import General from "./general/General";
+import Education from "./education/Education";
+import Work from "./work/Work";
+import Preview from "./preview/Preview";
+
+export {
+    Input,
+    Label,
+    Button,
+    General,
+    Education,
+    Work,
+    Preview
+}
