075397a160803af8e78bc509abb6bb57d03c517e
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-17 17:23:36 +0530

    Refactor App component layout to include Preview and organize sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import './styles/app.style.css';
+import { General, Education, Work, Preview } from './components';
+
 function App() {
 
     return (
 		<>
-			<h1>Hello Blue!</h1>
+			<main>
+				<div className="section-details">
+					<General></General>
+					<Education></Education>
+					<Work></Work>
+				</div>
+				<div className="section-preview">
+					<Preview></Preview>
+				</div>
+			</main>
 		</>
-    )
+    );
 }
 
 export default App;
